1aba6f448e969a20b8f8ea2fad9268fd0555e1f2
seohae <seohae0406@gmail.com>
2021-12-10 23:26:20 +0900

    (11.25 공부) 릿코드 문제풀이 1개

---
+++ src/SITE03_leetcode/easy/E004_leetCode58_LengthofLastWord.java
@@ -0,0 +1,33 @@
+package SITE03_leetcode.easy;
+
+import java.util.Objects;
+
+/**
+ * https://leetcode.com/problems/search-insert-position/
+ */
+public class E004_leetCode58_LengthofLastWord {
+    public static void main(String[] args) {
+        E004_leetCode58_LengthofLastWord solution = new E004_leetCode58_LengthofLastWord();
+
+        System.out.println(solution.lengthOfLastWord("   fly me   to   the moon  "));
+    }
+
+    public int lengthOfLastWord(String s) {
+        int result = 0;
+        boolean isInit = false;
+
+        for (int i = s.length() - 1; i >= 0; i--) {
+            if (!Objects.equals(String.valueOf(s.charAt(i)), " ")) {
+                isInit = true;
+                result++;
+            } else {
+                if (isInit) {
+                    break;
+                }
+            }
+        }
+
+        return result;
+    }
+
+}
