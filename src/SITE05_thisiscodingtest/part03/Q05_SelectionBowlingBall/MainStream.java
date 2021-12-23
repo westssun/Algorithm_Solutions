745cdb500f3e5b686cf37af7628f936ddf12fb40
seohae <seohae0406@gmail.com>
2021-12-23 22:44:31 +0900

    (12.23) Q05 stream 풀이

---
+++ src/SITE05_thisiscodingtest/part03/Q05_SelectionBowlingBall/MainStream.java
@@ -0,0 +1,72 @@
+package SITE05_thisiscodingtest.part03.Q05_SelectionBowlingBall;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import static java.util.stream.Collectors.toList;
+
+/*
+A , B
+두사람은 서로 다른 볼링공을 골라야한다.
+볼링공은 총 N개
+
+각 볼링공마다 무게가 있고, 공의 번호는 1번부터 N까지
+
+예시)
+N = 5, M = 3
+1 2 3 4 5
+1 3 2 3 2
+
+이때 두사람이 고를 수 있는 볼링공 번호의 조합은?
+(1, 2) -> (1, 3)
+(1, 3) -> (1, 2)
+(1, 4) -> (1, 3)
+(1, 5) -> (1, 2)
+(2, 3) -> (3, 2)
+(2, 4) -> (3, 3) -> 불가능
+(2, 5) -> (3, 2)
+(3, 4) -> (2, 3)
+(3, 5) -> (2, 2) -> 불가능
+(4, 5) -> (3, 2)
+
+-> 조합
+ */
+public class MainStream {
+    private static int N;
+    private static int M;
+    private static List<Integer> listA = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception {
+        input();
+
+        getPairs();
+    }
+
+    /*
+     5 3
+     1 3 2 3 2
+     */
+    private static void input() throws Exception {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        int M = scanner.nextInt();
+
+        for (int i = 1; i < N + 1; i++) {
+            int value = scanner.nextInt();
+
+            listA.add(value);
+        }
+    }
+
+    private static void getPairs() {
+
+        List<int[]> pairs = listA.stream()
+                .flatMap(i -> listA.stream()
+                        .filter(j -> i < j)
+                        .map(j -> new int[]{i, j}))
+                .collect(toList());
+
+        System.out.println(pairs.size());
+    }
+}
